e5e2b72b6dcc00353374fcca06e365167ff51dee
joaocastro125 <joaocastro_125@hotmail.com>
2023-06-03 18:33:37 -0300

    projeto html

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projeto html </title>
+</head>
+
+<body>
+
+    <a href="../../">Logomarca</a>
+    <ul>
+        <a href="#">
+            <li>Home</li>
+        </a>
+        <a href="">
+            <li>Depoimentos</li>
+        </a>
+        <a href="">
+            <li>Serviço</li>
+        </a>
+        <a href="">
+            <li>Contatos</li>
+        </a>
+    </ul>
+
+    <ul>
+        <a href="#">
+            <li>Home</li>
+        </a>
+        <a href="">
+            <li>Depoimentos</li>
+        </a>
+        <a href="">
+            <li>Serviço</li>
+        </a>
+        <a href="">
+            <li>Contatos</li>
+        </a>
+    </ul>
+
+    <form action="" method="get">
+        <h1>Qual o seu melhor email?</h1>
+        <input type="text" placeholder="Informe porfavor o seu email">
+        <input type="submit" value="Cadastrar">
+    </form>
+
+    <h1>João Castro</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo quae, a ducimus ullam eveniet numquam quam aliquid autem fugiat, libero soluta, sit placeat! Voluptates molestias corrupti quam explicabo quo est.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus impedit nisi facilis ad labore eos, tenetur illo pariatur recusandae incidunt molestiae sapiente mollitia commodi in maiores, eaque dolores? Nisi, saepe.
+    </p>
+    <img src="./img/foto.jpeg" alt="foto" width="300px">
+
+    <h1>Especialidades</h1>
+    <h3>Icone 1 </h3>
+    <h4>CSS3</h4>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, quae culpa sunt labore laboriosam consequatur accusantium ut numquam molestiae sint. Inventore, quod voluptatibus nihil earum iure dolor eum? Soluta?</p>
+
+    <h3>Icone 2</h3>
+
+    <h4>HTML5</h4>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, quae culpa sunt labore laboriosam consequatur accusantium ut numquam molestiae sint. Inventore, quod voluptatibus nihil earum iure dolor eum? Soluta?</p>
+
+    <h3>Icone 3</h3>
+
+    <h4>PHP </h4>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, quae culpa sunt labore laboriosam consequatur accusantium ut numquam molestiae sint. Inventore, quod voluptatibus nihil earum iure dolor eum? Soluta?</p>
+
+    <h1>Depoimentos dos nossos Clientes</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, dolorem provident officia nihil, in magnam, neque eaque id consequuntur ipsam voluptatem deserunt commodi ullam vero corporis accusamus nesciunt doloribus animi.</p>
+
+    <h1>Serviços</h1>
+    <ul>
+        <li>Lorem ipsum dolor sit amet</li>
+        <li>Lorem ipsum dolor sit amet</li>
+        <li>Lorem ipsum dolor sit amet</li>
+    </ul>
+    <p>Todos os direitos reservados!</p>
+
+</body>
+
+</html>
